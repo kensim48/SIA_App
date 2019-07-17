934c63acf390afa743ef4d8bbb102a4640296604
kensim48 <kensim28@hotmail.com>
2019-07-17 18:52:48 +0800

    Added page numbers

--- frontend_vendor/src/views/Dashboard.vue
+++ frontend_vendor/src/views/Dashboard.vue
@@ -23,8 +23,41 @@
         </v-flex>
       </v-layout>
     </div>
+    <div class="buttons">
+      <v-btn v-on:click="pageLeft()" dark large>
+        <v-icon dark large>arrow_left</v-icon>
+      </v-btn>
+      {{ pageNo }}
+      <v-btn v-on:click="pageRight()" dark large>
+        <v-icon dark large>arrow_right</v-icon>
+      </v-btn>
+    </div>
   </v-container>
 </template>
+
+
+<script>
+import SingleCard from "../components/SingleCard";
+
+export default {
+  components: {
+    SingleCard
+  },
+  data: () => ({
+    pageNo: 1,
+    maxPages: 6
+  }),
+  methods: {
+    pageRight() {
+      console.log("right");
+      this.pageNo<this.maxPages?this.pageNo++:console.log("fail");
+    },
+    pageLeft() {
+      this.pageNo>1?this.pageNo--:null;
+    }
+  }
+};
+</script>
 
 <style scoped>
 .overall {
@@ -47,20 +80,20 @@
 .bottom {
   bottom: 0;
   background-color: green;
+  padding-bottom: 3rem;
 }
 
 .layout {
   height: 100%;
 }
+
+.buttons {
+  position: fixed;
+  bottom: 0;
+  display: inline-block;
+  font-size: 2rem;
+  /* width: 400px; */
+  /* left: 45%; */
+  /* width: 75%; */
+}
 </style>
-
-<script>
-import SingleCard from "../components/SingleCard";
-
-export default {
-  components: {
-    SingleCard
-  }
-};
-</script>
-
