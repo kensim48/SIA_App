656f576457e6dacd02403772b473432b4afac3ae
Kenneth Sim <kensim28@hotmail.com>
2019-07-20 15:23:03 +0800

    added v-toolbar and v-navigationdrawer

--- frontend_vendor/src/views/Dashboard.vue
+++ frontend_vendor/src/views/Dashboard.vue
@@ -1,34 +1,60 @@
 <template>
-  <v-container class="overall" grid-list-xl text-xs-center>
-    <div class="top row">
-      <v-layout v-if="pageNo==1" class="layout">
-        <v-flex v-for="order in firstPageFirstRowOrders" v-bind:key="order.orderNum" xs6>
-          <DoubleCard :orderNum="order.orderNum" :meals="order.meals" />
-        </v-flex>
-      </v-layout>
-      <v-layout v-else class="layout">
-        <v-flex v-for="order in firstRowOrders" v-bind:key="order.orderNum" xs4>
-          <SingleCard :orderNum="order.orderNum" :meals="order.meals" />
-        </v-flex>
-      </v-layout>
-    </div>
-    <div class="bottom row">
-      <v-layout class="layout">
-        <v-flex v-for="order in secondRowOrders" v-bind:key="order.orderNum" xs4>
-          <SingleCard :orderNum="order.orderNum" :meals="order.meals" />
-        </v-flex>
-      </v-layout>
-    </div>
-    <div class="buttons">
-      <v-btn v-on:click="pageLeft()" dark large>
-        <v-icon dark large>arrow_left</v-icon>
-      </v-btn>
-      {{ pageNo }}
-      <v-btn v-on:click="pageRight()" dark large>
-        <v-icon dark large>arrow_right</v-icon>
-      </v-btn>
-    </div>
-  </v-container>
+  <div>
+    <v-toolbar app fixed clipped-left dark height="50">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" clipped hide-overlay fixed app disable-resize-watcher>
+      <v-list dense>
+        <v-list-tile @click>
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-container class="overall" fluid grid-list-xl text-xs-center>
+      <div class="top row">
+        <v-layout v-if="pageNo==1" class="layout">
+          <v-flex v-for="order in firstPageFirstRowOrders" v-bind:key="order.orderNum" xs6>
+            <DoubleCard :orderNum="order.orderNum" :meals="order.meals" />
+          </v-flex>
+        </v-layout>
+        <v-layout v-else class="layout">
+          <v-flex v-for="order in firstRowOrders" v-bind:key="order.orderNum" xs4>
+            <SingleCard :orderNum="order.orderNum" :meals="order.meals" />
+          </v-flex>
+        </v-layout>
+      </div>
+      <div class="bottom row">
+        <v-layout class="layout">
+          <v-flex v-for="order in secondRowOrders" v-bind:key="order.orderNum" xs4>
+            <SingleCard :orderNum="order.orderNum" :meals="order.meals" />
+          </v-flex>
+        </v-layout>
+      </div>
+      <div class="buttons">
+        <v-btn v-on:click="pageLeft()" dark large>
+          <v-icon dark large>arrow_left</v-icon>
+        </v-btn>
+        {{ pageNo }}
+        <v-btn v-on:click="pageRight()" dark large>
+          <v-icon dark large>arrow_right</v-icon>
+        </v-btn>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 
@@ -65,6 +91,7 @@ export default {
   data: () => ({
     pageNo: 1,
     maxPages: 6,
+    drawer: 0,
     orders: [
       {
         orderNum: "#123",
@@ -234,6 +261,7 @@ export default {
 <style scoped>
 .overall {
   height: 100%;
+
   background-color: brown;
 }
 
@@ -241,11 +269,11 @@ export default {
   position: fixed;
   left: 0;
   right: 0;
-  height: 50%;
+  height: calc(50% - (50px / 2));
 }
 
 .top {
-  top: 0;
+  top: 50px;
   background-color: orange;
 }
 
