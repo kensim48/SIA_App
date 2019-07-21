85e80711be4180ef4b2fb57921f86de43f51ca86
Kenneth Sim <kensim28@hotmail.com>
2019-07-21 19:33:41 +0800

    changed a number of items to text boxes

--- frontend_vendor/src/views/Menu.vue
+++ frontend_vendor/src/views/Menu.vue
@@ -11,21 +11,35 @@
       </v-layout>
     </v-container>
     <v-layout row justify-center>
-      <v-dialog v-model="itemDialog" width="1000px">
+      <v-dialog persistent v-model="itemDialog" width="600px">
         <v-card>
           <v-card-title>
-            <span class="headline">{{menuItems[selectedItem].title}}</span>
+            <v-text-field
+              style="width: 100%;"
+              v-model="menuItems[selectedItem].title"
+              label="Title of meal"
+              box
+            ></v-text-field>
+            <v-textarea
+              style="width: 100%;"
+              label="Description of meal"
+              v-model="menuItems[selectedItem].description"
+              box
+            ></v-textarea>
           </v-card-title>
           <div
             v-for="(question, indexQn) in menuItems[selectedItem].questions"
             :key="indexQn"
             class="question"
           >
-            {{ question.title }}
-            {{ question.description }}
-            <!-- description not showing -->
+            <hr style="margin-bottom: 20px;" />
             <v-layout row wrap>
-              <v-flex xs3>
+              <v-flex xs12>
+                <v-text-field
+                  v-model="menuItems[selectedItem].questions[indexQn].title"
+                  label="Title of question"
+                  outline
+                ></v-text-field>
                 <div v-if="question.type==0">(Select one)</div>
                 <div v-else-if="question.type==1">
                   <v-layout row wrap>
@@ -34,7 +48,6 @@
                       <v-select
                         v-model="menuItems[selectedItem].questions[indexQn].optionLimits[0]"
                         :items="minMaxItems"
-                        box
                         label="Minimum"
                       ></v-select>
                     </v-flex>
@@ -43,7 +56,6 @@
                       <v-select
                         v-model="menuItems[selectedItem].questions[indexQn].optionLimits[1]"
                         :items="maxItems(indexQn)"
-                        box
                         label="Maximum"
                       ></v-select>
                     </v-flex>
@@ -58,10 +70,10 @@
                 ></v-text-field>
                 <v-btn
                   fab
-                  dark
                   small
                   @click="menuItems[selectedItem].questions[indexQn].options.splice(indexOp,1)"
                   color="primary"
+                  :disabled="menuItems[selectedItem].questions[indexQn].options.length<=1"
                 >
                   <v-icon dark>remove</v-icon>
                 </v-btn>
@@ -77,7 +89,17 @@
           </div>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="green darken-1" flat="flat" @click="itemDialog = false">Disagree</v-btn>
+            <v-menu open-on-hover top offset-y>
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark v-on="on">Dropdown</v-btn>
+              </template>
+
+              <v-list>
+                <v-list-tile v-for="(type, index) in typesOfQuestion" :key="index" @click>
+                  <v-list-tile-title @click="createNewQuestion(index)">{{ type }}</v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+            </v-menu>
             <v-btn color="green darken-1" flat="flat" @click="itemDialog = false">Agree</v-btn>
           </v-card-actions>
         </v-card>
@@ -98,12 +120,26 @@ export default {
         this.menuItems[this.selectedItem].questions[indexQn].optionLimits[0] - 1
       );
     },
-    computed: {}
+    createNewQuestion(type) {
+      type == 0
+        ? this.menuItems[this.selectedItem].questions.push({
+            type: 0,
+            title: "",
+            options: [""]
+          })
+        : this.menuItems[this.selectedItem].questions.push({
+            type: 1,
+            optionLimits: [null, null],
+            title: "",
+            options: [""]
+          });
+    }
   },
   data: () => ({
     itemDialog: false,
     selectedItem: 0,
     minMaxItems: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    typesOfQuestion: ["Select one", "Select multiple"],
     menuItems: [
       {
         title: "asdasd",
@@ -165,6 +201,6 @@ export default {
   float: right;
 }
 .question {
-  display: inline;
+  padding: 2% 8%;
 }
 </style>
