3e38d330249bdbd62ff043222adc70d5e4eff82b
kensim48 <kensim28@hotmail.com>
2019-07-21 13:11:11 +0800

    Moved nav drawer to app.vue

--- frontend_vendor/src/views/Dashboard.vue
+++ frontend_vendor/src/views/Dashboard.vue
@@ -1,29 +1,5 @@
 <template>
   <div>
-    <v-toolbar app fixed clipped-left dark height="50">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-toolbar>
-    <v-navigation-drawer v-model="drawer" clipped hide-overlay fixed app disable-resize-watcher>
-      <v-list dense>
-        <v-list-tile @click>
-          <v-list-tile-action>
-            <v-icon>dashboard</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Dashboard</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click>
-          <v-list-tile-action>
-            <v-icon>settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Settings</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-container class="overall" fluid grid-list-xl text-xs-center>
       <div class="top row">
         <v-layout v-if="pageNo==1" class="layout">
